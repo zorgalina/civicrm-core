8d27bcf6454e02d37ce140cfba2491b39cf0bc62
zorgalina <zorgalina>
2015-08-03 21:13:46 -0700

    'CRM-16481'

--- CRM/Event/Cart/Form/Checkout/Payment.php
+++ CRM/Event/Cart/Form/Checkout/Payment.php
@@ -260,7 +260,7 @@ class CRM_Event_Cart_Form_Checkout_Payment extends CRM_Event_Cart_Form_Cart {
         if (array_search($event_in_cart->event_id, $discounted_event_ids) != NULL) {
           $this->apply_discount($discountcode, $price_set_amount, $cost);
         }
-      }   
+      }
       $cost = $event_price_values['amount'];
       $amount_level = $event_price_values['amount_level'];
       $price_details[$price_set_id] = $price_set_amount;
@@ -729,13 +729,13 @@ class CRM_Event_Cart_Form_Checkout_Payment extends CRM_Event_Cart_Form_Cart {
     return $defaults;
   }
   
-/**
+  /**
    * @param $discountcode
    * @param array $price_set_amount
    * @param &$cost
    *
    * @return void
-*/
+   */
   public function apply_discount($discountcode, &$price_set_amount, &$cost) {
     $discounts = array();
     $discounted = array();
@@ -745,7 +745,7 @@ class CRM_Event_Cart_Form_Checkout_Payment extends CRM_Event_Cart_Form_Cart {
     foreach ($discounts as $key => $discvalue) {
       if ($key == $discountcode) {
       //check priceset and is_active
-        $today = Date('Y-m-d');
+        $today = date('Y-m-d');
         $diff1 = date_diff(date_create($today), date_create($discvalue['active_on']));
         if ($diff1->days > 0) {
           $active1 = TRUE;
@@ -755,12 +755,12 @@ class CRM_Event_Cart_Form_Checkout_Payment extends CRM_Event_Cart_Form_Cart {
           if ($diff2->days > 0 ) {
             $active2 = TRUE;
           }
-        else {
-          $active2 = true;
+	} else {
+          $active2 = TRUE;
         }
         if ($discvalue['is_active'] == TRUE && ($discvalue['count_max'] == 0 || ($discvalue['count_max'] > $discvalue['count_use'])) && $active1 == TRUE && $active2 == TRUE)  {
-          foreach ($price_set_amount as $key=>$price) {
-            if ( array_search($price['price_field_value_id'],$discounted_priceset_ids) != NULL) {
+          foreach ($price_set_amount as $key => $price) {
+            if (array_search($price['price_field_value_id'], $discounted_priceset_ids) != NULL) {
               $discounted = _cividiscount_calc_discount($price['line_total'], $price['label'], $discvalue, $autodiscount, "USD");
               $price_set_amount[$key]['line_total'] = $discounted[0];
               $cost += $discounted[0];
